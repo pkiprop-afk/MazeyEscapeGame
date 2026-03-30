db2dd96c7b54345034db2e61164c7d8185b23ffb
Peter Kiprop <PKiprop@Caldwell.edu>
2026-03-30 23:41:52 +0000

    ${30PM7:41 PMe7:41 PMi41e}

---
+++ MazeGameCode/Mazey-1-Sprite.py
@@ -0,0 +1,20 @@
+import pygame
+import sys 
+
+# CONSTANTS
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+TITLE = "MAZEY"
+TITLE_SIZE = 60
+
+#COLORS
+# COLORS
+BLACK = (0, 0, 0)
+GRAY = (150, 150, 150)
+WHITE = (255, 255, 255)
+
+
+pygame.init() # --> Initializing Pygame
+
+# CONSTANTS
+GAME_TITLE = "Welcome to my World"
