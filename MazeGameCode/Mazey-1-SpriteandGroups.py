d42716ff4f7d76ae41a6a5b1a7c01ef45972a96d
Peter Kiprop <PKiprop@Caldwell.edu>
2026-03-31 05:34:53 +0000

    ${31AM1:34 AMe1:34 AMi34e}

--- MazeGameCode/Mazey-1-SpriteandGroups.py
+++ MazeGameCode/Mazey-1-SpriteandGroups.py
@@ -23,8 +23,6 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((TILE_SIZE - 8, TILE_SIZE - 8))     #--> defines what the sprite looks like
         self.image.fill(BLUE)       # -->
         
-        pygame.draw.rect(self.image, (), self.image.get_rect(), 3)      #--> Drawing a dark border on the wall to make the tiles visually distinct
-        
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
         
@@ -48,6 +46,8 @@ class Wall(pygame.sprite.Sprite):
         
         self.image = pygame.Surface((TILE_SIZE - 8, TILE_SIZE - 8))     #--> defines what the sprite will look like
         self.image.fill(BLUE)
+        
+        pygame.draw.rect(self.image, (), self.image.get_rect(), 3)      #--> Drawing a dark border on the wall to make the tiles visually distinct
         
         
         self.rect = self.image.get_rect()   
