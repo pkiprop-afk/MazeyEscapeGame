b0e8236d1816df96290ffab1b48fe6ba35f23b0e
Peter Kiprop <PKiprop@Caldwell.edu>
2026-03-31 06:30:29 +0000

    ${31AM2:30 AMe2:30 AMi30e}

--- MazeGameCode/Mazey-3-collision.py
+++ MazeGameCode/Mazey-3-collision.py
@@ -1,4 +1,17 @@
 import pygame
 import sys
 
-# Constants
+# CONSTANTS
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+TITLE = "MAZEY"
+MAX_FPS = 60        #--> 60 fps
+
+#COLORS
+BLACK = (0, 0, 0)
+GRAY = (150, 150, 150)
+WHITE = (255, 255, 255)
+BLUE = (50, 100, 255)
+GREEN = ( 60, 200, 60)
+YELLOW = (255, 220, 0)
+RED = (220, 60, 60)
