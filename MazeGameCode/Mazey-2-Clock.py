7c86e170b75cc92076b262746281d990239ad6e9
Peter Kiprop <PKiprop@Caldwell.edu>
2026-03-31 05:44:58 +0000

    ${31AM1:44 AMe1:44 AMi44e}

--- MazeGameCode/Mazey-2-Clock.py
+++ MazeGameCode/Mazey-2-Clock.py
@@ -7,4 +7,11 @@ SCREEN_HEIGHT = 600
 TITLE = "MAZEY"
 MAX_FPS = 60        #--> 60 fps
 
+#COLORS
+BLACK = (0, 0, 0)
+GRAY = (150, 150, 150)
+WHITE = (255, 255, 255)
+BLUE = (50, 100, 255)
+GREEN = ( 60, 200, 60)
+YELLOW = (255, 220, 0)
 
