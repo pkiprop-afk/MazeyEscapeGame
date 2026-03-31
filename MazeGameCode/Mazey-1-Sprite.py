1e9fb41e370f41bfe6f0e974b4f60c984e6094d6
Peter Kiprop <PKiprop@Caldwell.edu>
2026-03-31 04:52:41 +0000

    ${31AM12:52 AMe12:52 AMi52e}

--- MazeGameCode/Mazey-1-Sprite.py
+++ MazeGameCode/Mazey-1-Sprite.py
@@ -55,6 +55,7 @@ class Wall(pygame.sprite.Sprite):
 pygame.init() # --> Initializing Pygame
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption(TITLE)
+font = pygame.font.Sysfont("Arial", 20)
 
 # CONSTANTS
 GAME_TITLE = "Welcome to my World"
