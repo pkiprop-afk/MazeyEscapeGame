15974d769bcf0b50d805e974b3bdb55110f1ce73
Peter Kiprop <PKiprop@Caldwell.edu>
2026-04-06 10:01:04 +0000

    ${6AM6:01 AMe6:01 AMi1e}

--- MazeGameCode/draw_func.py
+++ MazeGameCode/draw_func.py
@@ -1,3 +1,15 @@
 import random
 
 import pygame
+
+from .Mazey_constants import(
+    FLOOR_DARK,
+    FLOOR_LIGHT, 
+    SCREEN_HEIGHT,
+    SCREEN_WIDTH,
+    TILE_SIZE,
+)
+
+def draw_floor(surface, x, y):
+    pygame.draw.rect(surface, FLOOR_DARK, (x, y, TILE_SIZE, TILE_SIZE))
+    
