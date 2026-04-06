6c2a888182bfbaf613fdc33f8539d2a8d857acc4
Peter Kiprop <PKiprop@Caldwell.edu>
2026-04-06 10:38:31 +0000

    ${6AM6:38 AMe6:38 AMi38e}

--- MazeGameCode/Mazey_builder.py
+++ MazeGameCode/Mazey_builder.py
@@ -2,7 +2,7 @@ import random
 
 import pygame
 
-from Mazey_constants import ALL_POWERUPS, DIFFICULTY, TILE_SIZE
+from Mazey_constants import ALL_POWERUPS, DIFFICULTY, TILE_SIZE, MAP_DATA
 from Mazey_spritesandgroups import Goal, GuardEnemy, PatrolEnemy, Player, Powerup, Wall
 
 def build_map(difficulty_key):
@@ -17,3 +17,8 @@ def build_map(difficulty_key):
     patrol_list = []
     guard_list = []
     
+    for row_indx, row_txt in enumerate(MAP_DATA):
+        for col_indx, tile_char in enumerate(row_txt):
+            x = col_indx * TILE_SIZE
+            y = row_indx * TILE_SIZE
+            
