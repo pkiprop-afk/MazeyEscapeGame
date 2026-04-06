400035ed159bb92f143db680356cab05061ef49f
Peter Kiprop <PKiprop@Caldwell.edu>
2026-04-06 10:35:15 +0000

    ${6AM6:35 AMe6:35 AMi35e}

--- MazeGameCode/Mazey_builder.py
+++ MazeGameCode/Mazey_builder.py
@@ -6,4 +6,14 @@ from Mazey_constants import ALL_POWERUPS, DIFFICULTY, TILE_SIZE
 from Mazey_spritesandgroups import Goal, GuardEnemy, PatrolEnemy, Player, Powerup, Wall
 
 def build_map(difficulty_key):
-    settin
+    settings = DIFFICULTY[difficulty_key]
+    
+    walls = pygame.sprite.Group()
+    goals = pygame.sprite.Group()
+    all_enemies = pygame.sprite.Group()
+    powerups = pygame.sprite.Group()
+    floor_rects = []
+    player = None
+    patrol_list = []
+    guard_list = []
+    
