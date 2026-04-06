4be698cb8bd363c3b704ea0ab8d70c9954079789
Peter Kiprop <PKiprop@Caldwell.edu>
2026-04-06 10:05:11 +0000

    ${6AM6:05 AMe6:05 AMi5e}

--- MazeGameCode/draw_func.py
+++ MazeGameCode/draw_func.py
@@ -8,8 +8,14 @@ from .Mazey_constants import(
     SCREEN_HEIGHT,
     SCREEN_WIDTH,
     TILE_SIZE,
+    BLACK,
 )
 
 def draw_floor(surface, x, y):
     pygame.draw.rect(surface, FLOOR_DARK, (x, y, TILE_SIZE, TILE_SIZE))
-    
+    pygame.draw.rect(surface, FLOOR_LIGHT, (x, y + TILE_SIZE  // 2, TILE_SIZE, 1))
+    pygame.draw.rect(surface, FLOOR_LIGHT, (x + TILE_SIZE // 2, y, 1, TILE_SIZE))
+
+def draw_flashlight(screen, player_center, radius, battery):
+    darkness = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SRCALPHA)
+    darkness.fill(BLACK)
