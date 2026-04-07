83f9eafc851eca24933204bf4fa64df5afa3d37a
Peter Kiprop <PKiprop@Caldwell.edu>
2026-04-07 04:25:36 +0000

    ${7AM12:25 AMe12:25 AMi25e}

--- src/Mazey_game.py
+++ src/Mazey_game.py
@@ -3,5 +3,26 @@ import sys
 
 from Mazey_constants import (
     DARK_BACKGROUND,
-    
+    DARK_GREY,
+    DIFFICULTY,
+    GRAY,
+    GREEN,
+    ORANGE, 
+    MAX_FPS,
+    PLAYER_COL,
+    PLAYER_EYE,
+    PU_AUTO_WIN,
+    PU_COLORS,
+    RED,
+    SCREEN_HEIGHT,
+    SCREEN_WIDTH,
+    STATE_DIFFICULT,
+    STATE_GAMEOVER, 
+    STATE_LOADING,
+    STATE_PLAYING,
+    STATE_TITLE,
+    STATE_WIN,
+    TITLE,
+    WHITE,
+    YELLOW,
 )
