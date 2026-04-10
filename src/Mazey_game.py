c03c0a1b352f71d2be99a813f63748b3054a93ac
Peter Kiprop <PKiprop@Caldwell.edu>
2026-04-10 21:16:38 +0000

    ${10PM5:16 PMe5:16 PMi16e}

--- src/Mazey_game.py
+++ src/Mazey_game.py
@@ -335,4 +335,6 @@ class Game:
             col = PU_COLORS.get(self.player.active_powerup, WHITE)
             self.screen.blit(self.font_small.render(f"{self.player.active_powerup} {self.player.powerup_timer:.1f}s", True, col), (SCREEN_WIDTH - 280, 10))
         
-        
+        # The weapon and bullet count
+        if self.player.weapon:
+            self.screen.blit
