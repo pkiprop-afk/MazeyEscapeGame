27bcffe40616e3502634a3459090f158c3c6aab7
Peter Kiprop <PKiprop@Caldwell.edu>
2026-04-11 00:10:40 +0000

    ${10PM8:10 PMe8:10 PMi10e}

--- src/Mazey_game.py
+++ src/Mazey_game.py
@@ -76,7 +76,7 @@ class Game:
         
         self.bullets = pygame.sprite.Group()
         self.all_sprites = pygame.sprite.Group()
-        self.all_sprites.add(self.goals.sprites(), self.powerups.sprites(), self.all_enemies, self.player)
+        self.all_sprites.add(self.goals.sprites(), self.powerups.sprites(), self.all_enemies.sprites(), self.player)
         self.time_left = float(DIFFICULTY[self.difficult]["time"])
         self.state = STATE_PLAYING
     
