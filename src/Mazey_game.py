c8d72222c3aa394e27239b92eb6385ce92a95bee
Peter Kiprop <PKiprop@Caldwell.edu>
2026-04-13 02:23:34 +0000

    ${12PM10:23 PMe10:23 PMi23e}

--- src/Mazey_game.py
+++ src/Mazey_game.py
@@ -48,7 +48,7 @@ class Game:
             music_path = os.path.join(curr_dir, "assets", "soundtrack.mp3")
             
             pygame.mixer.music.load(music_path)
-            pygame.mixer.music.set_volume(0.2)
+            pygame.mixer.music.set_volume(0.1)
             pygame.mixer.music.play(-1)
             
         except FileNotFoundError:
